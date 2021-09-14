c401f75fb59fc9cdd17e88ee857ff0360c62a2df
winone <100winone@naver.com>
2021-09-14 20:19:26 +0900

    Init Commit

---
+++ src/main/java/chapter05/day01/ArrayEx2.java
@@ -0,0 +1,13 @@
+package chapter05.day01;
+
+import java.util.*;
+
+public class ArrayEx2 {
+    public static void main(String[] args) {
+        int[] iArr1 = new int[10];
+        int[] iArr2 = new int[10];
+        int[] iArr3 = new int[]{100, 95, 80, 70, 60};
+        int[] iArr4 = {100, 95, 80, };
+
+    }
+}
