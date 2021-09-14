023af2a664a0db6c84640c7ed752a6122d02c486
winone <100winone@naver.com>
2021-09-15 00:04:30 +0900

    Day1 (2021.09.14) Commit

---
+++ src/main/java/chapter05/day01/ArrayEx10.java
@@ -0,0 +1,12 @@
+package chapter05.day01;
+
+public class ArrayEx10 {
+    public static void main(String[] args) {
+        int[] numArr = new int[10];
+//        for (int i = 0; i < numArr.length; i++) {
+//            System.out.print(numArr[i] +);
+//        }
+        int a;
+        System.out.print(a = 1 + 2);
+    }
+}
