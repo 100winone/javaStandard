8c543a212b4afbf9f9671d1a28990e82245b5c50
winone <100winone@naver.com>
2021-09-14 20:26:11 +0900

    Day1 (2021.09.14) Commit

--- src/main/java/chapter05/day01/ArrayEx2.java
+++ src/main/java/chapter05/day01/ArrayEx2.java
@@ -7,7 +7,23 @@ public class ArrayEx2 {
         int[] iArr1 = new int[10];
         int[] iArr2 = new int[10];
         int[] iArr3 = new int[]{100, 95, 80, 70, 60};
-        int[] iArr4 = {100, 95, 80, };
+        int[] iArr4 = {100, 95, 80, 70, 60};
+        char[] chArr = {'a', 'b', 'c', 'd'};
 
+        for (int i = 0; i < iArr1.length; i++) {
+            iArr1[i] = i + 1;
+        }
+        for (int i = 0; i < iArr2.length; i++) {
+            iArr2[i] = (int) (Math.random() * 10) + 1;
+        }
+        for (int i = 0; i < iArr1.length; i++) {
+            System.out.print(iArr1[i] + ",");
+        }
+        System.out.println();
+        System.out.println(Arrays.toString(iArr2));
+        System.out.println(Arrays.toString(iArr3)); // [100, 95, 80, 70, 60]
+        System.out.println(Arrays.toString(chArr)); // [a, b, c, d]
+        System.out.println(iArr4);
+        System.out.println(chArr); // abcd
     }
 }
